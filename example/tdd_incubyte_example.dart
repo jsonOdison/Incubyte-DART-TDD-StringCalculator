99009ee8861b132609f30ca2cf57dae212e5a7cc
Jason khongwir <50296658+jsonOdison@users.noreply.github.com>
2025-09-20 05:23:52 -0700

    added code cleanup

--- example/tdd_incubyte_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:tdd_incubyte/tdd_incubyte.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
---
+++ lib/src/tdd_incubyte_main.dart
@@ -0,0 +1,10 @@
+void main() {
+  var calculator = Calculator();
+  print(calculator.add('1,2,3')); // Output: 6
+}
+
+class Calculator {
+  int add(String numbers) {
+    return 0;
+  }
+}
